5076433a65ea2078e958207c671e2911cb833f6a
Flyynn <banjamin.wollert@gmail.com>
2018-02-18 16:29:54 +0100

    Rectangle hype

--- gettingOverIsabelle/gamewindow.cpp
+++ gettingOverIsabelle/gamewindow.cpp
@@ -3,13 +3,21 @@
 #include "ui_gamewindow.h"
 #include <QMovie>
 #include <QLabel>
+#include <QGraphicsView>
+
 
 GameWindow::GameWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::GameWindow)
 {
     ui->setupUi(this);
+
     connect(ui->actionMenu_Principal,SIGNAL(triggered(bool)),this,SLOT(backToMainWin()));
+
+    QGraphicsScene *scene = new QGraphicsScene(this);
+    scene->addRect(QRectF(0,0,200,100));
+
+    ui->graphicsView->setScene(scene);
 }
 
 GameWindow::~GameWindow()
